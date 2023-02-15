93440e8a43d4fd1b136df7e8000e7aa72853b0e4
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-02-15 10:15:24 +0530

    auth completed

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from "@/lib/prisma";
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        EmailProvider({
+            server: {
+                host: process.env.SMTP_HOST,
+                port: Number(process.env.SMTP_PORT),
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+        }),
+    ],
+})
